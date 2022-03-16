920cf3a1c5df41776f3531f8ea685f053be607da
Pol Henarejos <pol.henarejos@cttc.es>
2022-03-16 23:31:10 +0100

    Upgrading to v1.2
    Signed-off-by: Pol Henarejos <pol.henarejos@cttc.es>

--- src/hsm/version.h
+++ src/hsm/version.h
@@ -1,7 +1,7 @@
 #ifndef __VERSION_H_
 #define __VERSION_H_
 
-#define HSM_VERSION 0x0101
+#define HSM_VERSION 0x0102
 
 #define HSM_VERSION_MAJOR ((HSM_VERSION >> 8) & 0xff)
 #define HSM_VERSION_MINOR (HSM_VERSION & 0xff)
