c4f06cceadf1fa0aae0370e2feb19bd22110208d
Pol Henarejos <pol.henarejos@cttc.es>
2022-06-01 08:58:58 +0200

    Not used anymore.

--- src/openpgp/affine.h
+++
@@ -1,8 +0,0 @@
-/**
- * @brief	Affine coordinates
- */
-typedef struct
-{
-  bn256 x[1];
-  bn256 y[1];
-} ac;
--- src/openpgp/mod.h
+++
@@ -1,3 +0,0 @@
-void mod_reduce (bn256 *X, const bn512 *A, const bn256 *B,
-		 const bn256 *MU_lower);
-void mod_inv (bn256 *X, const bn256 *A, const bn256 *N);
