7e6ed20b2675e32cbfc3f731776ce09945dfde12
Pol Henarejos <pol.henarejos@cttc.es>
2022-08-18 23:53:49 +0200

    Not downloading nested submodules for tinyusb
    Signed-off-by: Pol Henarejos <pol.henarejos@cttc.es>

--- workflows/autobuild.sh
+++ workflows/autobuild.sh
@@ -5,7 +5,7 @@ sudo apt update
 sudo apt install -y cmake gcc-arm-none-eabi libnewlib-arm-none-eabi libstdc++-arm-none-eabi-newlib
 git clone https://github.com/raspberrypi/pico-sdk
 cd pico-sdk
-git submodule update --init --recursive
+git submodule update --init
 cd ..
 mkdir build
 cd build
