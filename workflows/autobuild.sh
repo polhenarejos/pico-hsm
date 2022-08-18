84ba0e03de348574a8d87e2e4b6586d197849ece
Pol Henarejos <pol.henarejos@cttc.es>
2022-08-18 23:31:17 +0200

    Fix missing TinyUSB submodule
    Signed-off-by: Pol Henarejos <pol.henarejos@cttc.es>

--- workflows/autobuild.sh
+++ workflows/autobuild.sh
@@ -3,6 +3,9 @@
 sudo apt update
 sudo apt install -y cmake gcc-arm-none-eabi libnewlib-arm-none-eabi libstdc++-arm-none-eabi-newlib
 git clone https://github.com/raspberrypi/pico-sdk
+cd pico-sdk
+git submodule update --init
+cd ..
 mkdir build
 cd build
 cmake -DPICO_SDK_PATH=../pico-sdk ..
