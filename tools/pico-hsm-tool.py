1a6cfd17cbd581fce4d2697c73fafecf0d023279
Reiner Rottmann <reiner@rottmann.it>
2022-10-30 08:51:57 +0100

    Small fix in ModuleNotFoundError handling.

--- tools/pico-hsm-tool.py
+++ tools/pico-hsm-tool.py
@@ -24,17 +24,17 @@ try:
     from smartcard.CardType import AnyCardType
     from smartcard.CardRequest import CardRequest
     from smartcard.Exceptions import CardRequestTimeoutException
-    from cvc.certificates import CVC
-except:
+except ModuleNotFoundError:
     print('ERROR: smarctard module not found! Install pyscard package.\nTry with `pip install pyscard`')
     sys.exit(-1)
 
 try:
+    from cvc.certificates import CVC
     from cvc.asn1 import ASN1
     from cvc.oid import oid2scheme
     from cvc.utils import scheme_rsa
     from cryptography.hazmat.primitives.asymmetric import ec
-except:
+except ModuleNotFoundError:
     print('ERROR: cvc module not found! Install pycvc package.\nTry with `pip install pycvc`')
     sys.exit(-1)
 import json
