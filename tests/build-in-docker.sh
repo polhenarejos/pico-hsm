307eb63ee5ac126f754e91465b3ca7bc463c80d4
Pol Henarejos <pol.henarejos@cttc.es>
2023-01-30 12:59:03 +0100

    Init submodules in actions.
    Signed-off-by: Pol Henarejos <pol.henarejos@cttc.es>

--- tests/build-in-docker.sh
+++ tests/build-in-docker.sh
@@ -2,8 +2,6 @@
 
 source tests/docker_env.sh
 #run_in_docker rm -rf CMakeFiles
-run_in_docker git config --global --add safe.directory '*'
-run_in_docker git submodule update --recursive
 run_in_docker mkdir -p build_in_docker
 run_in_docker -w "$PWD/build_in_docker" cmake -DENABLE_EMULATION=1 ..
 run_in_docker -w "$PWD/build_in_docker" make -j ${NUM_PROC}
