3b0e34f4654baffe153cdd793f4ec1c8af084d19
Pol Henarejos <pol.henarejos@cttc.es>
2023-01-30 13:23:39 +0100

    More tries...
    Signed-off-by: Pol Henarejos <pol.henarejos@cttc.es>

--- tests/start-up-and-test.sh
+++ tests/start-up-and-test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -eu
 
 /usr/sbin/pcscd -f -d &
-./build_in_docker/pico_hsm &
-pytest tests -W ignore::DeprecationWarning
+./build_in_docker/pico_hsm
+#pytest tests -W ignore::DeprecationWarning
