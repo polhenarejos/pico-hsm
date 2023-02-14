962d1c69168e3876f1f5f82364194f711c164af6
Pol Henarejos <pol.henarejos@cttc.es>
2023-02-14 22:53:30 +0100

    When runing a test, load a initial flash image that contains KEYDEV.
    Signed-off-by: Pol Henarejos <pol.henarejos@cttc.es>

--- tests/run-test-in-docker.sh
+++ tests/run-test-in-docker.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -eu
 
 source tests/docker_env.sh
-run_in_docker rm -f memory.flash
 run_in_docker ./tests/start-up-and-test.sh
 
--- tests/start-up-and-test.sh
+++ tests/start-up-and-test.sh
@@ -2,5 +2,7 @@
 
 /usr/sbin/pcscd &
 sleep 2
+rm -f memory.flash
+tar -xf tests/memory.tar.gz
 ./build_in_docker/pico_hsm > /dev/null &
 pytest tests -W ignore::DeprecationWarning
