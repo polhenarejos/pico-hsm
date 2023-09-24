d906df280953e7e0925d6bf91ea7c4fb1632a23c
Pol Henarejos <pol.henarejos@cttc.es>
2023-09-24 10:30:59 +0200

    Add initialization test script with pkcs11
    Signed-off-by: Pol Henarejos <pol.henarejos@cttc.es>

--- tests/scripts/initialize.sh
+++ tests/scripts/initialize.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -eu
+#!/bin/bash
 
 python3 tools/pico-hsm-tool.py --pin 648219 initialize --so-pin 57621880 --silent
 test $? -eq 0 || exit $?
@@ -40,5 +40,5 @@ grep -q CKR_PIN_LOCKED <<< $e || exit $?
 pkcs11-tool --login --login-type so --so-pin 3537363231383830 --init-pin --new-pin 648219
 test $? -eq 0 || exit $?
 
-pkcs11-tool --login --pin 648219 -I > /dev/null
+pkcs11-tool --login --pin 648219 -I
 test $? -eq 0 || exit $?
--- tests/start-up-and-test.sh
+++ tests/start-up-and-test.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -eu
+#!/bin/bash
 
 rm -rf pypicohsm
 git clone https://github.com/polhenarejos/pypicohsm.git
@@ -7,12 +7,11 @@ pip3 install -e pypicohsm
 sleep 2
 rm -f memory.flash
 tar -xf tests/memory.tar.gz
-./build_in_docker/pico_hsm > /dev/null &
-#pytest tests -W ignore::DeprecationWarning
+./build_in_docker/pico_hsm > /dev/null 2>&1 &
+pytest tests -W ignore::DeprecationWarning
 
 chmod a+x tests/scripts/*.sh
 
 echo -n "Test initialization... "
-./tests/scripts/initialize.sh
-echo "\tok"
-
+./tests/scripts/initialize.sh > /dev/null 2>&1
+test $? -eq 0 && echo -e '\tok' || (echo -e '\tfail' && exit 1)
