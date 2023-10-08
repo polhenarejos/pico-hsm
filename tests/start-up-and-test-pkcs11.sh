c3af303468ed30aa2ebb15d715e9cb3de4c9ed37
Pol Henarejos <pol.henarejos@cttc.es>
2023-10-09 00:58:49 +0200

    Split pytest and pkcs11 test suites.
    Signed-off-by: Pol Henarejos <pol.henarejos@cttc.es>

--- tests/start-up-and-test-pkcs11.sh
+++ tests/start-up-and-test-pkcs11.sh
@@ -8,4 +8,8 @@ sleep 2
 rm -f memory.flash
 tar -xf tests/memory.tar.gz
 ./build_in_docker/pico_hsm > /dev/null 2>&1 &
-pytest tests -W ignore::DeprecationWarning
+
+chmod a+x tests/scripts/*.sh
+
+echo "======== PKCS11 Test suite ========"
+./tests/scripts/pkcs11.sh
