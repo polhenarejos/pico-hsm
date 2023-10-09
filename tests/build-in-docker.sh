b74c4070d14332a496691f9346590fc5016afedc
Pol Henarejos <pol.henarejos@cttc.es>
2023-10-09 18:26:16 +0200

    When build, it copies generated binaries to image, which will be used later by other jobs.
    Signed-off-by: Pol Henarejos <pol.henarejos@cttc.es>

--- tests/build-in-docker.sh
+++ tests/build-in-docker.sh
@@ -6,3 +6,9 @@ build_image
 run_in_docker mkdir -p build_in_docker
 run_in_docker -w "$PWD/build_in_docker" cmake -DENABLE_EMULATION=1 ..
 run_in_docker -w "$PWD/build_in_docker" make -j ${NUM_PROC}
+docker create --name temp_container pico-hsm-test:bullseye
+docker cp $PWD/build_in_docker/pico_hsm temp_container:/pico_hsm
+docker commit temp_container pico-hsm-test:bullseye
+docker stop temp_container
+docker rm temp_container
+docker rmi $(docker images -f "dangling=true" -q)
