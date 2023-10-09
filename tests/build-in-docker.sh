d5af9160c1039e65e7a6191cfc5e02fb9f5dacd5
Pol Henarejos <pol.henarejos@cttc.es>
2023-10-09 21:55:55 +0200

    Build in docker with for CI flag.
    Signed-off-by: Pol Henarejos <pol.henarejos@cttc.es>

--- tests/build-in-docker.sh
+++ tests/build-in-docker.sh
@@ -4,7 +4,7 @@ source tests/docker_env.sh
 build_image
 #run_in_docker rm -rf CMakeFiles
 run_in_docker mkdir -p build_in_docker
-run_in_docker -w "$PWD/build_in_docker" cmake -DENABLE_EMULATION=1 ..
+run_in_docker -w "$PWD/build_in_docker" cmake -DENABLE_EMULATION=1 -D__FOR_CI=1 ..
 run_in_docker -w "$PWD/build_in_docker" make -j ${NUM_PROC}
 docker create --name temp_container pico-hsm-test:bullseye
 docker cp $PWD/build_in_docker/pico_hsm temp_container:/pico_hsm
