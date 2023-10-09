a7770a7e571c9cb7e17a1455be4deb356aead64d
Pol Henarejos <pol.henarejos@cttc.es>
2023-10-09 18:25:10 +0200

    Check Pico HSM PCSC connectivity.
    Signed-off-by: Pol Henarejos <pol.henarejos@cttc.es>

--- tests/startup.sh
+++ tests/startup.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-OK="\033[32mok\033[0m"
-FAIL="\033[31mfail\033[0m"
+OK="\t\033[32mok\033[0m"
+FAIL="\t\033[31mfail\033[0m"
 
-echo -n "Start PCSC... "
+fail() {
+    echo -e "${FAIL}"
+    exit 1
+}
+
+echo -n "Start PCSC..."
 /usr/sbin/pcscd &
 test $? -eq 0 && echo -e "${OK}" || {
     echo -e "${FAIL}"
@@ -12,10 +17,11 @@ test $? -eq 0 && echo -e "${OK}" || {
 sleep 2
 rm -f memory.flash
 tar -xf tests/memory.tar.gz
-echo -n "Start Pico HSM... "
-./build_in_docker/pico_hsm &
-test $? -eq 0 && echo -e "${OK}" || {
-    echo -e "${FAIL}"
-    exit 1
-}
+echo -n "Start Pico HSM..."
+/pico_hsm > /dev/null 2>&1 &
+test $? -eq 0 && echo -n "." || fail
 sleep 2
+ATR="3b:fe:18:00:00:81:31:fe:45:80:31:81:54:48:53:4d:31:73:80:21:40:81:07:fa"
+e=$(opensc-tool -an 2>&1)
+grep -q "${ATR}" <<< $e && echo -n "." || fail
+test $? -eq 0 && echo -e "${OK}" || fail
