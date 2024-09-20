4454c633e4db00d6aad27aa587c3778d28e2e543
Pol Henarejos <pol.henarejos@cttc.es>
2024-09-20 07:16:55 +0200

    Autobuild esp32
    Signed-off-by: Pol Henarejos <pol.henarejos@cttc.es>

--- workflows/autobuild.sh
+++ workflows/autobuild.sh
@@ -2,6 +2,8 @@
 
 git submodule update --init --recursive
 sudo apt update
+
+if [[ $1 == "pico" ]]; then
 sudo apt install -y cmake gcc-arm-none-eabi libnewlib-arm-none-eabi libstdc++-arm-none-eabi-newlib
 git clone https://github.com/raspberrypi/pico-sdk
 cd pico-sdk
@@ -9,9 +11,20 @@ git submodule update --init
 cd ..
 mkdir build
 cd build
-if [[ $1 == "pico" ]]; then
 cmake -DPICO_SDK_PATH=../pico-sdk ..
-else
-cmake -DENABLE_EMULATION=1 ..
-fi
 make
+elif [[ $1 == "esp32" ]]; then
+sudo apt install -y git wget flex bison gperf python3 python3-pip python3-venv cmake ninja-build ccache libffi-dev libssl-dev dfu-util libusb-1.0-0
+git clone --recursive https://github.com/espressif/esp-idf.git
+cd esp-idf
+./install.sh esp32s3
+. ./export.sh
+cd ..
+idf.py set-target esp32s3
+idf.py all
+else
+mkdir build
+cd build
+cmake -DENABLE_EMULATION=1 ..
+make
+fi
