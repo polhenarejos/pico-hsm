b5b5ccd53cb568a33c7d4ad2d528705060e58a1f
Pol Henarejos <pol.henarejos@cttc.es>
2025-12-01 17:21:26 +0100

    Upgrade to v6.0
    Signed-off-by: Pol Henarejos <pol.henarejos@cttc.es>

--- src/hsm/version.h
+++ src/hsm/version.h
@@ -18,7 +18,7 @@
 #ifndef __VERSION_H_
 #define __VERSION_H_
 
-#define HSM_VERSION 0x0506
+#define HSM_VERSION 0x0600
 
 #define HSM_VERSION_MAJOR ((HSM_VERSION >> 8) & 0xff)
 #define HSM_VERSION_MINOR (HSM_VERSION & 0xff)
