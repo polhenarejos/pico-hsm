82f4b2201ccfbfa1f2604010b141f66e934b0f65
Pol Henarejos <pol.henarejos@cttc.es>
2025-12-09 21:38:15 +0100

    Remove printf
    Signed-off-by: Pol Henarejos <pol.henarejos@cttc.es>

--- src/hsm/cmd_keypair_gen.c
+++ src/hsm/cmd_keypair_gen.c
@@ -47,9 +47,6 @@ int cmd_keypair_gen() {
                 if (asn1_find_tag(&ctxo, 0x2, &ks) && asn1_len(&ks) > 0) {
                     key_size = asn1_get_uint(&ks);
                 }
-                printf("KEYPAIR RSA %lu (%lx)\n",
-                       (unsigned long) key_size,
-                       (unsigned long) exponent);
                 mbedtls_rsa_context rsa;
                 mbedtls_rsa_init(&rsa);
                 uint8_t index = 0;
@@ -74,7 +71,6 @@ int cmd_keypair_gen() {
                     return SW_WRONG_DATA();
                 }
                 mbedtls_ecp_group_id ec_id = ec_get_curve_from_prime(prime.data, prime.len);
-                printf("KEYPAIR ECC %d\n", ec_id);
                 if (ec_id == MBEDTLS_ECP_DP_NONE) {
                     return SW_FUNC_NOT_SUPPORTED();
                 }
@@ -84,7 +80,6 @@ int cmd_keypair_gen() {
                         return SW_WRONG_DATA();
                     }
                 }
-                printf("KEYPAIR ECC %d\r\n", ec_id);
                 mbedtls_ecdsa_context ecdsa;
                 mbedtls_ecdsa_init(&ecdsa);
                 uint8_t index = 0;
