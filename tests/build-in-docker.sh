0b1594c5a7757aaa482b537afc004ab0187b76bf
Pol Henarejos <pol.henarejos@cttc.es>
2026-07-14 12:06:47 +0200

    Switch to bookworm test.
    Signed-off-by: Pol Henarejos <pol.henarejos@cttc.es>

--- tests/build-in-docker.sh
+++ tests/build-in-docker.sh
@@ -6,9 +6,9 @@ build_image
 run_in_docker mkdir -p build_in_docker
 run_in_docker -w "$PWD/build_in_docker" cmake -DENABLE_EMULATION=1 -D__FOR_CI=1 -DENABLE_EDDSA=1 ..
 run_in_docker -w "$PWD/build_in_docker" make -j ${NUM_PROC}
-docker create --name temp_container pico-hsm-test:bullseye
+docker create --name temp_container pico-hsm-test:bookworm
 docker cp $PWD/build_in_docker/pico_hsm temp_container:/pico_hsm
-docker commit temp_container pico-hsm-test:bullseye
+docker commit temp_container pico-hsm-test:bookworm
 docker stop temp_container
 docker rm temp_container
 docker image prune -f
